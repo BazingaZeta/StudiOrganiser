cc81ad11bed7f8ac16d50affa2fd7a3c779a4abc
Edoardo Trotta <Edoardo.Trotta@clinisys.co.uk>
2019-12-20 11:22:58 +0000

    Updates

---
+++ precache-manifest.17b8fbcad7891515d0e0b63bd8158594.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2ffdf99a7b4aa08edf5c00ce94045c",
+    "url": "/StudiOrganiser/index.html"
+  },
+  {
+    "revision": "d2a9ac37f01f48211395",
+    "url": "/StudiOrganiser/static/css/main.750371f3.chunk.css"
+  },
+  {
+    "revision": "e6310ddeee3daa15e8c5",
+    "url": "/StudiOrganiser/static/js/2.15a2bac5.chunk.js"
+  },
+  {
+    "revision": "d2a9ac37f01f48211395",
+    "url": "/StudiOrganiser/static/js/main.75a49bb6.chunk.js"
+  },
+  {
+    "revision": "54b421ef74852df3ceb6",
+    "url": "/StudiOrganiser/static/js/runtime-main.36ab35c4.js"
+  }
+]);
